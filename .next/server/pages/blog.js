58594281f1c19fbe1956da47c6fc1e5fb10f0895
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 12:24:09 +0000

    5

--- .next/server/pages/blog.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/blog.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1a141b9e._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__84687bd7._.js")
-R.c("server/chunks/ssr/_053ad5c6._.js")
-R.c("server/chunks/ssr/[externals]_react_jsx-runtime_7ad7b9a3._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__2574aea5._.js")
-R.c("server/chunks/ssr/pages__app_43455d5d.js")
-R.m(82549)
-module.exports=R.m(82549).exports
--- .next/server/pages/servicos.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/servicos.js")
-R.c("server/chunks/ssr/[root-of-the-server]__eaec2748._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__84687bd7._.js")
-R.c("server/chunks/ssr/_0cdfb137._.js")
-R.c("server/chunks/ssr/[externals]_react_jsx-runtime_7ad7b9a3._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__2574aea5._.js")
-R.c("server/chunks/ssr/pages__app_43455d5d.js")
-R.m(85551)
-module.exports=R.m(85551).exports
