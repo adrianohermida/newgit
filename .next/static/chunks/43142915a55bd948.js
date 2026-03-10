bf503b4f7af522532de6c6a4cd9fd05db44d5960
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 08:08:45 +0000

    1

--- .next/static/chunks/43142915a55bd948.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/a970da8bae9b65b1.js",
-  "static/chunks/688648673823d495.js",
-  "static/chunks/turbopack-7e15d07fcfadff9c.js"
-])
--- .next/static/chunks/fdcb2bfc8c974372.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/7c0e73682b015377.js",
-  "static/chunks/688648673823d495.js",
-  "static/chunks/turbopack-a72bca0d37a718de.js"
-])
