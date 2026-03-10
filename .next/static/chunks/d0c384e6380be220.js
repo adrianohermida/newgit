e7cc10a84a4c825ed7780bf7c1efdc9ea45a20f4
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 12:13:24 +0000

    45

---
+++ .next/static/chunks/d0c384e6380be220.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/servicos", [
+  "static/chunks/227956914bae5ff9.js",
+  "static/chunks/1917bdb3bc691503.js",
+  "static/chunks/204e2614e99eb52b.js",
+  "static/chunks/turbopack-aadd1e571c54e5e1.js"
+])
