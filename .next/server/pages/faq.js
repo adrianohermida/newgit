4d05232fc9f1d78d98fb71d239d92c3e08280db1
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 08:45:39 +0000

    Remove obsolete SSR chunk file from the server build

---
+++ .next/server/pages/faq.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/faq.js")
+R.c("server/chunks/ssr/node_modules_next_dist_esm_build_templates_pages_88221212.js")
+R.c("server/chunks/ssr/[root-of-the-server]__33abe4b8._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__7a085623._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__b3ba77a0._.js")
+R.c("server/chunks/ssr/_f8bf07a9._.js")
+R.m(8340)
+module.exports=R.m(8340).exports
---
+++ .next/static/chunks/4c8e47c5da290154.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/faq", [
+  "static/chunks/5f39edf6f7f29e11.js",
+  "static/chunks/54d48b21aac31d9a.js",
+  "static/chunks/da0ff8adaa6cce90.js",
+  "static/chunks/turbopack-d51e5dd0d3083636.js"
+])
