3957d754529ce3da3a7a9797e065095f0c9690d4
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 10:13:57 +0000

    Add new Turbopack chunks for optimized loading of blog and other resources
    - Introduced new JavaScript chunks for the blog page and other components to enhance performance.
    - Implemented dynamic loading of additional chunks to improve resource management.
    - Ensured compatibility with the Turbopack runtime for efficient module handling.

---
+++ .next/static/chunks/e647fe37b8735b36.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/servicos", [
+  "static/chunks/9fcea4c4d3e5060a.js",
+  "static/chunks/093c964a8322c347.js",
+  "static/chunks/215ffbe8665ad1a1.js",
+  "static/chunks/turbopack-3973250d5163dbf8.js"
+])
