2282c6c366c2b5bf99407d95644ab9d8b0e3e09b
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 09:56:57 +0000

    1

---
+++ .next/server/pages/servicos.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/servicos.js")
+R.c("server/chunks/ssr/node_modules_next_dist_esm_build_templates_pages_8fe05fed.js")
+R.c("server/chunks/ssr/[root-of-the-server]__33abe4b8._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__7a085623._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__b3ba77a0._.js")
+R.c("server/chunks/ssr/_f8bf07a9._.js")
+R.m(5551)
+module.exports=R.m(5551).exports
---
+++ .next/static/chunks/fc64f63aaf203c9b.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/blog", [
+  "static/chunks/dd1ac600d5cf1b6a.js",
+  "static/chunks/093c964a8322c347.js",
+  "static/chunks/215ffbe8665ad1a1.js",
+  "static/chunks/turbopack-12439e290979eff0.js"
+])
