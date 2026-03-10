0825d4d0f4311b83735fe17ec5efdab21708cf74
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 08:44:39 +0000

    1

--- .next/server/pages/servicos.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/servicos.js")
-R.c("server/chunks/ssr/node_modules_next_dist_esm_build_templates_pages_8fe05fed.js")
-R.c("server/chunks/ssr/[root-of-the-server]__33abe4b8._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__68d47023._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__62249c69._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__3383d4ef._.js")
-R.m(5551)
-module.exports=R.m(5551).exports
--- .next/static/chunks/57d9eb0b3163363c.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/43b6f6d30c056fa7.js",
-  "static/chunks/9339d63570f4495e.js",
-  "static/chunks/a7ae9040ac1c94e3.js",
-  "static/chunks/6853252d9a69a9ab.css",
-  "static/chunks/turbopack-63bebf6199ae64cc.js"
-])
