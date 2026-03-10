88a9d6b279927ae6d1256943d0cef0574ec4968d
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-10 08:36:57 +0000

    Remove unused Turbopack chunks and associated build files to clean up the project structure.

--- .next/static/chunks/546b7a55d894b371.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/4b2a6ce6949ad37d.js",
-  "static/chunks/9339d63570f4495e.js",
-  "static/chunks/a7ae9040ac1c94e3.js",
-  "static/chunks/turbopack-f848aba4a995bcbd.js"
-])
