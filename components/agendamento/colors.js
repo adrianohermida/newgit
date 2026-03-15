49a1936936cd62544199a07aff9746fd8fd9a6f9
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-15 23:45:42 +0000

    feat: adicionar paleta de cores centralizada para componentes

---
+++ components/agendamento/colors.js
@@ -0,0 +1,5 @@
+// Paleta de cores centralizada para uso em todos os componentes
+export const GOLD = "#C5A059";
+export const OBSIDIAN = "#050706";
+export const PARCHMENT = "#F4F1EA";
+export const GHOST = "#2D2E2E";
