b525bf1ad5ba7681eb1a1a4af1fcef7f0c64c6f0
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-03-16 09:21:48 +0000

    1

--- pages/api/slots.js
+++ pages/api/slots.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const oAuth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    process.env.GOOGLE_CALENDAR_REDIRECT_URI
   );
   // Sempre obter novo access token via refresh token
   let accessToken = null;
