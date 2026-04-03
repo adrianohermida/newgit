a4d410ac532d1d4da87e5081df1aef79ff1c89f9
Adriano Hermida Maia <adrianohermida@gmail.com>
2026-04-03 09:39:01 -0300

    4

--- pages/_app.js
+++ pages/_app.js
@@ -69,7 +69,7 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Escritório de advocacia especializado em superendividamento, revisão bancária, contratos, defesa contra juros abusivos, empréstimo consignado, cartão de crédito e direito bancário." />
         <meta name="keywords" content="advogado, superendividamento, revisão bancária, contratos, juros abusivo, empréstimo consignado, cartão de crédito, reserva de margem consignada, defesa do consumidor, negociação de dívidas, recuperação judicial, direito bancário, consultoria jurídica" />
         <link rel="icon" type="image/webp" href="/images/OIP.webp" />
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#07110E" />
         {/* Google Tag Manager Head (GTM-TMBHHW6 e GTM-56WQHDR) */}
         <script dangerouslySetInnerHTML={{ __html: `
